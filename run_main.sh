91c9ff19c62bb00a87f01395fe6fa157ca203078
czfmy800 <167760469+czfmy800@users.noreply.github.com>
2024-12-25 21:30:40 +0800

    Update run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -1,19 +1,26 @@
-model_id=yolov10s
-model_path=yolov10s.pt
-datasets="coco"
+model_id=yolov10x
+model_path=yolov10x.pt
+datasets="tt100k_2021"
+epoch=100
 mkdir -p out_log
+
+export CUDA_LAUNCH_BLOCKING=1
 
 # 使用 nohup 运行训练脚本，并将输出重定向到 out_log/train.log
 nohup python main.py \
     --model_id "$model_id" \
     --model_path "Yolov10/$model_path" \
-    --epoch 1 \
-    --project "./$model_id-$datasets" \
+    --epoch $epoch \
+    --project "./result_file/$model_id-$datasets-$epoch" \
     --batch 16 \
     --imgsz 640 \
     --device '0' \
-    --worker 4 \
+    --worker 8 \
     --train \
+    --eval \
     --datasets $datasets \
-    --train_result_file "./$model_id-$datasets/result-$model_id-$datasets" \
-    > out_log/train-$model_id-$datasets-.log 2>&1 &
+    --lr0 0.01 \
+    --eval_datasets "tt100k_2021" \
+    --train_result_file "./result/$model_id-$datasets/result-$model_id-$datasets" \
+    > out_log/train-$model_id-$datasets-$epoch-debug.log 2>&1 &
+
